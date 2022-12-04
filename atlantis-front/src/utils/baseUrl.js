ef9d30c0fbf45986ac15c46af758090939b3dd67
Frank <812359326@qq.com>
2022-12-04 23:43:11 +0800

    22/12/04: modify README and baseUrl

--- atlantis-front/src/utils/baseUrl.js
+++ atlantis-front/src/utils/baseUrl.js
@@ -1,7 +1,7 @@
 // 统一定义后端访问路径
 
-const baseURL = 'http://localhost:8080/api';
-// const baseURL = 'http://localhost:8081/api';
+// const baseURL = 'http://localhost:8080/api';
+const baseURL = 'http://localhost:8081/api';
 // const baseURL = 'http://localhost:8082/api';
 // const baseURL = 'http://localhost:8686/api';
 // const baseURL = 'http://172.16.10.210:8081/api';
